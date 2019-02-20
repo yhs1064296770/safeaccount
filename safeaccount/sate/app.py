458d1431d44390da8f13df9266979fefb47e8549
海深 <kaishin@haishens-iMac.local>
2019-02-20 13:42:02 +0800

    代码提交信息

---
+++ safeaccount/sate/app.py
@@ -0,0 +1,41 @@
+# coding: utf-8
+
+from datetime import datetime
+
+from flask import Flask
+from flask import render_template
+from flask_sockets import Sockets
+
+from views.todos import todos_view
+
+app = Flask(__name__)
+sockets = Sockets(app)
+
+# 动态路由
+app.register_blueprint(todos_view, url_prefix='/todos')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/time')
+def time():
+    return str(datetime.now())
+
+
+@sockets.route('/echo')
+def echo_socket(ws):
+    while True:
+        message = ws.receive()
+        ws.send(message)
+
+@app.route('/ys.html')
+def ys():
+    return render_template('yisi.html')
+
+
+@app.route('/lxwm.html')
+def lxwm():
+    return render_template('lianxiwomen.html')
